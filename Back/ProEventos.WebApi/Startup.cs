352aea2a12f0b6dbd1856887c3bf3013ee77cede
Gustavo M. Santos <gustavoms050@gmail.com>
2021-02-24 12:46:54 -0300

    Fixed some bugs

--- Back/ProEventos.WebApi/Startup.cs
+++ Back/ProEventos.WebApi/Startup.cs
@@ -30,6 +30,7 @@ namespace ProEventos.WebApi
         {
             services.AddDbContext<DataContext>(context=> context.UseSqlite(Configuration.GetConnectionString("Default")));
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.WebApi", Version = "v1" });
@@ -51,6 +52,8 @@ namespace ProEventos.WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(access=>access.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {

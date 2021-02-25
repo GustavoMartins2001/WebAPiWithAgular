dd80f23f081c031e13be3fdf3b542fdedb018d75
Gustavo M. Santos <gustavoms050@gmail.com>
2021-02-25 16:02:19 -0300

    Added search Function

--- Front/ProEventos-app/src/app/app.module.ts
+++ Front/ProEventos-app/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
       EventosComponent,
-      PalestrantesComponent
+      PalestrantesComponent,
+      NavComponent,
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
     NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-app/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-app/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { isWhileStatement } from 'typescript';
 
 @Component({
   selector: 'app-eventos',
@@ -7,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
+  isExpanded:boolean =false;
+  imageWidth:number = 80;
+  imageMargin:number = 2;
+  public eventos : any = [];
+  public eventosFiltrados: any = [];
+  private _filtro:string ='';
 
+  public get filtro(){
+    return this._filtro;
+  }
 
-  public eventos : any;
+public set filtro(value:string){
+  this._filtro = value;
+  this.eventosFiltrados =this.filtro ? this.filtrarEventos(this.filtro) : this.eventos
+}
 
-  constructor(private http: HttpClient) { }
+public filtrarEventos(filtrarPor: string): any {
+  filtrarPor = filtrarPor.toLocaleLowerCase();
+  return this.eventos.filter(
+   (evento:any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !==- 1
+    )
+
+}
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
@@ -19,7 +40,18 @@ export class EventosComponent implements OnInit {
 
   public getEventos():void {
     this.http.get("https://localhost:5001/api/eventos")
-    .subscribe(response=> this.eventos = response, error =>console.log(error));
+    .subscribe(response=> {this.eventos = response
+                           this.eventosFiltrados = response}, error =>console.log(error));
+  }
+  public expandImage():void{
+    if(!this.isExpanded){
+      this.imageWidth = 200;
+    }
+    else{
+      this.imageWidth = 80;
+    }
+
+    this.isExpanded=!this.isExpanded;
 
   }
 
